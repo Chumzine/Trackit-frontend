ad0544abff99a7590e8f531e392d8f440c25a77f
Adaobi Chinasa Chuma-Okeke <86481843+Chumzine@users.noreply.github.com>
2024-04-26 22:24:52 +0100

    Update index.jsx

--- src/app/component/Pages/ProfileSettings/index.jsx
+++ src/app/component/Pages/ProfileSettings/index.jsx
@@ -35,6 +35,10 @@ export const Profile = () => {
                 body: JSON.stringify(profile)
             });
 
+            if (!response.ok) {
+                throw new Error('Failed to update. Please try again later.');
+            }
+
             // Handle successful update
             alert('Update successful!');
 
