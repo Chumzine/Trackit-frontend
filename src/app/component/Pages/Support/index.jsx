73b2974aa2dbb9b4ba1f31b102c44442e527c979
Asheklm6 <asheklm6@gmail.com>
2024-05-24 17:56:26 +0100

    updated styling and updated jsx

--- src/app/component/Pages/Support/index.jsx
+++ src/app/component/Pages/Support/index.jsx
@@ -1,28 +1,44 @@
+import React from 'react';
 import Logo from '../../../assets/images/logo.png';
 import { CustomSideNav } from "../../CustomSideNav/CustomSideNavigation";
-import { MainHeader } from '../../MainHeader';
 import { ReactComponent as CallUsIcon } from '../../../assets/icons/call-us.svg';
 import { ReactComponent as EmailUsIcon } from '../../../assets/icons/email-us.svg';
 import { ReactComponent as ChatIcon } from '../../../assets/icons/chat.svg';
 import { ReactComponent as SocialMediaIcon } from '../../../assets/icons/social-media.svg';
+import { IoMdNotificationsOutline, IoIosArrowDown } from 'react-icons/io';
+import { CgProfile } from 'react-icons/cg';
 import style from './style.module.css';
 
 export const Support = () => {
     return (
         <>
-        <header>
-            <img src={Logo} alt="Trackit logo" />
-        </header>
-        <main>
-            <CustomSideNav />
-            <div className={style.container}>
-                <div>
-                    <MainHeader>
-                        <h2>Support</h2>
-                    </MainHeader>
+            <header>
+                <img src={Logo} alt="Trackit logo" />
+            </header>
+
+            <div className={style.sideNavContainer}>
+                <CustomSideNav />
+            </div>
+
+            <main>
+                <p className={style.supportText}>Support</p>
+
+                <div className={style.profile}>
+                    <div className={style.icon}>
+                        <div className={style.profileIconContainer}>
+                            <CgProfile />
+                        </div>
+                        <h6>Munirah Subair</h6>
+                        <IoIosArrowDown />
+                    </div>
                 </div>
-                <div className={style['content-container']}>
-                    <div className={style.flexone}>
+
+                <div className={style.notificationIcon}>
+                    <IoMdNotificationsOutline />
+                </div>
+
+                <div className={style.container}>
+                    <div className={style['content-container']}>
                         <div className={style.call}>
                             <CallUsIcon />
                             <div>
@@ -37,14 +53,12 @@ export const Support = () => {
                                 <p>support@trackit.com</p>
                             </div>
                         </div>
-                    </div>
-                    <div className={style.flextwo}>
                         <div className={style.chat}>
                             <ChatIcon />
                             <div>
                                 <h5>Chat</h5>
                                 <p>9AM-5PM.M-F</p>
-                            </div> 
+                            </div>
                         </div>
                         <div className={style.socials}>
                             <SocialMediaIcon />
@@ -53,10 +67,9 @@ export const Support = () => {
                                 <p>@trackit</p>
                             </div>
                         </div>
-                    </div>    
+                    </div>
                 </div>
-            </div>
-        </main>
+            </main>
         </>
-    )
-}
+    );
+};
