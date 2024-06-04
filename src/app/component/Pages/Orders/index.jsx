cdd59d450a51e71eeb31fe222b27e8822236192e
Asheklm6 <asheklm6@gmail.com>
2024-06-04 17:12:06 +0100

    updated files for styling

--- src/app/component/Pages/Orders/index.jsx
+++ src/app/component/Pages/Orders/index.jsx
@@ -1,66 +1,67 @@
-import Logo from '../../../assets/images/logo.png'
+import React from "react";
+import Logo from '../../../assets/images/logo.png';
 import style from "./style.module.css";
-import { CustomSideNav} from "../../CustomSideNav/CustomSideNavigation"
+import { CustomSideNav } from "../../CustomSideNav/CustomSideNavigation";
 import { Link } from "react-router-dom";
-// import home from "../Assets/images/home.svg";
-// import note from "../Assets/images/note.svg";
-// import emptywallet from "../Assets/images/emptywallet.svg";
-// import setting from "../Assets/images/setting.svg";
-// import messagequestion from "../Assets/images/messagequestion.svg";
 import EmptyState from "../../../assets/icons/EmptyState.svg";
-// import Ellipse from "../Assets/images/Ellipse.svg";
-// import CustomButton from "../../CustomButton/CustomButton";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
-// import { FiPlus } from "react-icons/fi";
 
 export const Orders = () => {
   return (
-    <div className={style.containerDiv}>
-      <div>
-        <img src={Logo} alt="trackit logo" />
-      </div>
-      <div className={style.flexContainer}>
-        <div className={style.navigation}>
+    <>
+      <header className={style.header}>
+        <img src={Logo} alt="Trackit logo" className={style.logo} />
+    
+      </header>
+
+      <div className={style.container}>
+        <div className={style.sideNavContainer}>
           <CustomSideNav />
         </div>
-        <div className={style.welcome}>
-          <h4>Orders</h4>
-          <div className={style.para}>
-            <Link className={style.link}>Pending Orders</Link>
-            <div className={style.line}></div>
-            <Link className={style.link}>Completed Orders</Link>
-          </div>
-        </div>
-        <div className={style.profile}>
-          <div className={style.icon}>
-            <div>
-              <IoMdNotificationsOutline />
-            </div>
-            <div>
-              <CgProfile />
-            </div>
-            <h6>Munirah Subair</h6>
-            <div>
-              <IoIosArrowDown />{" "}
+        <main className={style.main}>
+          <div className={style.welcomeContainer}>
+            <div className={style.welcome}>
+              <h4>Orders</h4>
+              <div className={style.para}>
+                <Link className={`${style.link} ${style.active}`}>Pending Orders</Link>
+                <Link className={style.link}>Completed Orders</Link>
+              </div>
             </div>
           </div>
-        </div>
+
+          <div className={style.profile}>
+                    <div className={style.icon}>
+                        <div className={style.profileIconContainer}>
+                            <CgProfile />
+                        </div>
+                        <h6>Munirah Subair</h6>
+                        <IoIosArrowDown />
+                    </div>
+                </div>
+
+                <div className={style.notificationIcon}>
+                    <IoMdNotificationsOutline />
+                </div>
+                
+          <div className={style.deliveryInfoContainer}>
+            <div className={style.deliveryInfo}>
+              <Link className={style.info}>Date Placed</Link>
+              <Link className={style.info}>Tracking ID</Link>
+              <Link className={style.info}>Pickup Address</Link>
+              <Link className={style.info}>Dropoff Address</Link>
+              <Link className={style.info}>Phone Number</Link>
+              <Link className={style.info}>Price</Link>
+              <Link className={style.info}>Status</Link>
+            </div>
+            <div className={style.activity}>
+              <img src={EmptyState} alt="Empty box icon" />
+              <span>No Orders Placed</span>
+            </div>
+          </div>
+        </main>
       </div>
-      <div className={style.deliveryInfo}>
-        <Link className={style.info}>Date Placed</Link>
-        <Link className={style.info}>Tracking ID</Link>
-        <Link className={style.info}>Pickup Address</Link>
-        <Link className={style.info}>Dropoff Address</Link>
-        <Link className={style.info}>Phone Number</Link>
-        <Link className={style.info}> Price</Link>
-        <Link className={style.info}>Status</Link>
-      </div>
-      <div className={style.activity}>
-        <img src={EmptyState} alt="Empty box icon" />
-        <span>No orders Placed</span>
-      </div>
-    </div>
+    </>
   );
 };
